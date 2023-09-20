5c0095f51edfeba4cb79c2208134fce062999ad2
rafalgiemza <rgiemza@fandom.com>
2023-09-20 16:20:44 +0200

    add Hero v1.0

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">HR Auctions</h1>
+          <p className="py-6">Work in progress...</p>
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
