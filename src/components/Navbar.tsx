0f2f1d9598417e7b20720d0e6a9b81a21653be87
rafalgiemza <rgiemza@fandom.com>
2023-09-20 16:20:28 +0200

    add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">HR Auctions</a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <Image src="/avatar.jpg" height={844} width={844} alt="avatar" />
+            </div>
+          </label>
+          <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <li><Link href={'/login'}>logout</Link></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
