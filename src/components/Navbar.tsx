9e30bf89ff70e5587953e8238b8a351eef9de17b
rafalgiemza <rgiemza@fandom.com>
2023-09-21 16:34:26 +0200

    add link to main page in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">HR Auctions</a>
+        <Link href={"/"} className="btn btn-ghost normal-case text-xl">HR Auctions</Link>
       </div>
       <div className="flex-none gap-2">
         <ul className="menu menu-horizontal px-1">
