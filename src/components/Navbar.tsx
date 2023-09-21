3ba1fd2a001d104b1841dab030120542d6a758a4
rafalgiemza <rgiemza@fandom.com>
2023-09-21 16:33:39 +0200

    add link to /auctions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,9 @@ const Navbar = () => {
         <a className="btn btn-ghost normal-case text-xl">HR Auctions</a>
       </div>
       <div className="flex-none gap-2">
+        <ul className="menu menu-horizontal px-1">
+          <Link href={'/auctions'}>Auctions</Link>
+        </ul>
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
